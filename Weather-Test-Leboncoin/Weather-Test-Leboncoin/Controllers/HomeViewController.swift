84dffb5f3611bf695ffd86d04406894212b679a4
Hamza El Aidi <elaidi.hamza@gmail.com>
2020-01-05 17:04:03 +0100

    set up architecture

---
+++ Weather-Test-Leboncoin/Weather-Test-Leboncoin/Controllers/HomeViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Weather-Test-Leboncoin
+//
+//  Created by hamza on 1/3/20.
+//  Copyright © 2020 mac. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+
+}
+
---
+++ Weather-Test-Leboncoin/Weather-Test-Leboncoin/Misc/Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  Extensions.swift
+//  Weather-Test-Leboncoin
+//
+//  Created by hamza on 1/5/20.
+//  Copyright © 2020 mac. All rights reserved.
+//
+
+import Foundation
