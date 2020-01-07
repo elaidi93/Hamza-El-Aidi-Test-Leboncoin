5000d5ff152a4674176f6757e97bc36f4075bc78
Hamza El Aidi <elaidi.hamza@gmail.com>
2020-01-07 23:48:13 +0100

    implement location manager

--- Weather-Test-Leboncoin/Weather-Test-Leboncoin/Controllers/HomeViewController.swift
+++ Weather-Test-Leboncoin/Weather-Test-Leboncoin/Controllers/HomeViewController.swift
@@ -7,16 +7,28 @@
 //
 
 import UIKit
+import CoreLocation
 
 class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        instanciateLocationManager()
         
     }
 
+    func instanciateLocationManager() {
+        LocationManager.shared.requestAuthorization()
+        LocationManager.shared.startTracking()
+        LocationManager.shared.delegate = self
+    }
 
 }
 
+extension HomeViewController: LocationManagerDelegate {
+    func locationManager(didUpdate locations: [CLLocation]) {
+        let userLocation :CLLocation = locations[0] as CLLocation
+        print("\(userLocation.coordinate.latitude),\(userLocation.coordinate.longitude)")
+    }
+}
---
+++ Weather-Test-Leboncoin/Weather-Test-Leboncoin/Location Manager/LocationManager.swift
@@ -0,0 +1,48 @@
+//
+//  LocationManager.swift
+//  Weather-Test-Leboncoin
+//
+//  Created by hamza on 1/7/20.
+//  Copyright © 2020 mac. All rights reserved.
+//
+
+import CoreLocation
+
+protocol LocationManagerDelegate: class {
+    func locationManager(didUpdate locations: [CLLocation])
+}
+
+class LocationManager: NSObject, CLLocationManagerDelegate {
+    static let shared = LocationManager()
+    private var locationManager = CLLocationManager()
+    weak var delegate: LocationManagerDelegate?
+    
+    private override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    
+    func requestAuthorization() {
+        locationManager.requestAlwaysAuthorization()
+    }
+    
+    func startTracking() {
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    func stopTracking() {
+        locationManager.stopUpdatingHeading()
+        locationManager.stopUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        delegate?.locationManager(didUpdate: locations)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Error \(error.localizedDescription)")
+    }
+}
